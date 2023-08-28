5b52010d314ece3bd0877c4947aec4cce6048ab2
gooreumgil <gooreumgil47@gmail.com>
2023-08-28 21:42:25 +0900

    initial commit

---
+++ src/test/java/memory/MemoryV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package memory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoryV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
